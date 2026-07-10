ca6aea1d0c09a63cb12745ca5c89764d2f2319a4
VenturingForth <joshua@codeadventure.uk>
2026-07-10 22:17:55 +0100

    implement content authoring service

---
+++ VenturingForth.CMS.Application/Services/ContentAuthoringService.cs
@@ -0,0 +1,31 @@
+﻿using VenturingForth.CMS.Application.Interfaces;
+
+namespace VenturingForth.CMS.Application;
+
+public class ContentAuthoringService : IContentAuthoringService
+{
+    /// <inheritdoc/>
+    public async Task<Result<ContentItemResponse>> CreateAsync(CreateContentItemRequest request, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public async Task<Result<ContentItemResponse>> PublishAsync(Guid contentItemId, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public async Task ArchiveAsync(Guid contentItemId, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public async Task<Result<ContentItemResponse>> AssignTagsAsync(Guid contentItemId, IReadOnlyList<Guid> tagIds,
+        CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
