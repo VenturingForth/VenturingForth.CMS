784c4bfaa05c3f6afdbdef76510be685e248d384
VenturingForth <joshua@codeadventure.uk>
2026-07-10 22:27:24 +0100

    add program build instructions

--- VenturingForth.CMS.Api/Program.cs
+++ VenturingForth.CMS.Api/Program.cs
@@ -4,3 +4,7 @@ using VenturingForth.CMS.Application.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IContentAuthoringService, ContentAuthoringService>();
+
+var app = builder.Build();
+
+app.Run();
